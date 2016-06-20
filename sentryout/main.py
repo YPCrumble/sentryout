62dda329da025d31bb50d440238b2a08c1a67d47
Matt Oberle <moberle@nypublicradio.org>
2016-06-20 13:57:37 -0400

    simplify stdout/stderr handling in message

--- sentryout/main.py
+++ sentryout/main.py
@@ -1,5 +1,4 @@
 import argparse
-import io
 import os
 import raven
 import sys
@@ -9,6 +8,7 @@ try:
     from ConfigParser import RawConfigParser, NoSectionError
 except ImportError:
     from configparser import RawConfigParser, NoSectionError
+
 
 def send_to_sentry(args, stdout, stderr, exitcode, client_factory=raven.Client, extra={}):
 
@@ -32,20 +32,13 @@ def send_to_sentry(args, stdout, stderr, exitcode, client_factory=raven.Client, 
     # send to sentry
     if exitcode != 0 or args.ignore_exitcode:
         extra['exitcode'] = exitcode
-        if not args.ignore_stdout:
-            extra['stdout'] = stdout
-        if not args.ignore_stderr:
-            extra['stderr'] = stderr
-        if not args.ignore_environment:
-            for key, value in os.environ.items():
-                extra['env.' + key] = value
-        client.capture('Message',
-                       message=args.message,
-                       data = None,
-                       extra = extra)
+        for key, value in os.environ.items():
+            extra['env.' + key] = value
+        msg = "{}\n\nstdout:\n{}\n\nstderr:\n{}".format(args.message, stdout, stderr)
+        client.captureMessage(msg, extra=extra)
 
 def main():
-    parser = argparse.ArgumentParser(prog='sentryout', description='dump command line sentry logger')
+    parser = argparse.ArgumentParser(prog='sentryout', description='simple sentry cli wrapper')
 
     parser.add_argument('-p', '--project', metavar='PROJECT',
                         required=True, type=str,
@@ -58,23 +51,15 @@ def main():
     parser.add_argument('-e', '--cmd', metavar='CMD',
                         required=True, type=str,
                         help='bash command or script to execute')
+
     parser.add_argument('-c', '--config', metavar='PATH', type=str,
                         default=os.path.join(os.path.expanduser('~'), '.sentryout.conf'),
                         help='location of Sentry configuration file')
 
-    parser.add_argument('--ignore-environment',action='store_true',
-                        help='do not send environment variables to sentry')
-
     parser.add_argument('--ignore-exitcode', action='store_true',
                         help='send results to sentry regardless of exit code')
 
-    parser.add_argument('--ignore-stderr', action='store_true',
-                        help='do not send stderr output to sentry')
-
-    parser.add_argument('--ignore-stdout', action='store_true',
-                        help='do not send stdout output to sentry')
-
-    parser.add_argument('-v', '--version', action='version', version='0.0.1')
+    parser.add_argument('-v', '--version', action='version', version='0.0.3')
 
     args = parser.parse_args()
 
